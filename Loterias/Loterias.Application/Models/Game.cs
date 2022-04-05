a7536d5e5860ed580cde4be5d745086bbf70f046
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-05 16:29:22 -0300

    Criação de classe para representação do jogo e conversão implícita.

---
+++ Loterias/Loterias.Application/Models/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace Loterias.Application.Models
+{
+    public class Game
+    {
+        public string GameDrawn { get; set; }
+        public DateTime DrawDate { get; set; }
+
+        public static implicit operator Game(string data)
+        {
+            var gameData = data.Split(";");
+            var drawDate = gameData[0].Split("/").Select(x => int.TryParse(x, out int result) ? result : 0).ToArray();
+            string gameDrawn = data.Replace(gameData[0] + ";", "").Replace(";", "-");
+
+            return new Game
+            {
+                DrawDate = new DateTime(drawDate[2], drawDate[1], drawDate[0]),
+                GameDrawn = gameDrawn
+            };
+        }
+    }
+}
