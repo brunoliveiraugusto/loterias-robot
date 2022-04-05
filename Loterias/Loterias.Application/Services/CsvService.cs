021a8dacf4e925bde2576b27e3dd94a0af3a04ec
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-05 16:29:36 -0300

    Criação de serviço para leitura de csv contendo jogos.

---
+++ Loterias/Loterias.Application/Services/CsvService.cs
@@ -0,0 +1,24 @@
+﻿using Loterias.Application.Models;
+using Loterias.Application.Services.Interfaces;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Loterias.Application.Services
+{
+    public class CsvService : ICsvService
+    {
+        public IEnumerable<Game> Read()
+        {
+            var data = File.ReadLines($@"C:\Projetos\Pessoal\Loterias\loterias-robot\Loterias\Loterias.Application\FilesCsv\megasena.csv");
+            var games = new List<Game>();
+
+            foreach (var line in data)
+            {
+                Game game = line;
+                games.Add(game);
+            }
+
+            return games;
+        }
+    }
+}
---
+++ Loterias/Loterias.Application/Services/Interfaces/ICsvService.cs
@@ -0,0 +1,10 @@
+﻿using Loterias.Application.Models;
+using System.Collections.Generic;
+
+namespace Loterias.Application.Services.Interfaces
+{
+    public interface ICsvService
+    {
+        IEnumerable<Game> Read();
+    }
+}
