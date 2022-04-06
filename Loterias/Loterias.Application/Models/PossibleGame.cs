603e7588801cd6603b0ef5ef2e802e27007d1d07
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-06 10:12:14 -0300

    Criação de modelo para representar possível jogo a ser sorteado.

---
+++ Loterias/Loterias.Application/Models/PossibleGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Loterias.Application.Models
+{
+    public class PossibleGame
+    {
+        public string Number { get; set; }
+        public IEnumerable<IEnumerable<string>> LaterNumbers { get; set; }
+        public IEnumerable<string> PossibleNumbers { get; set; }
+
+        public PossibleGame()
+        {
+            LaterNumbers = new List<List<string>>();
+            PossibleNumbers = new List<string>();
+        }
+    }
+}
