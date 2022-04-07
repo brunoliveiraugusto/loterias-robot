5a4a95cfac32e3079ebad3f52918eaceee136961
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-07 20:27:08 -0300

    Injeção de dependência.

--- Loterias/Loterias.Robot/Startup.cs
+++ Loterias/Loterias.Robot/Startup.cs
@@ -1,19 +1,45 @@
 using Hangfire;
 using Hangfire.MemoryStorage;
+using Loterias.Application.Services;
+using Loterias.Application.Services.Interfaces;
+using Loterias.Application.Utils.Request;
+using Loterias.Application.Utils.Request.Interfaces;
+using Loterias.Application.Utils.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System.Net.Http;
 
 namespace Loterias.Robot
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<IMainService, MainService>();
+            services.AddTransient<ICsvService, CsvService>();
+            services.AddTransient<IGameService, GameService>();
+            services.AddTransient<IScrapingService, ScrapingService>();
+            services.AddTransient<IHttpService, HttpService>();
+            services.AddSingleton<HttpClient>();
+
+            services.Configure<GameRequest>(
+                Configuration.GetSection(nameof(GameRequest)));
+
             services.AddHangfire(op =>
             {
                 op.UseMemoryStorage();
             });
+
+            GlobalConfiguration.Configuration.UseSerilogLogProvider();
 
             services.AddHangfireServer();
         }
@@ -21,6 +47,8 @@ namespace Loterias.Robot
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             app.UseHangfireDashboard();
+
+            RecurringJob.AddOrUpdate<IMainService>(managerService => managerService.Execute(), Cron.Daily);
         }
     }
 }
