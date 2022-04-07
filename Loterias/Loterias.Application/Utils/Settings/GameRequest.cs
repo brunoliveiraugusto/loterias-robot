7513c6493bf06240c89cc8b0819094d018b8a537
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-07 20:27:23 -0300

    Alteração de estrutura de variáveis de ambiente.

---
+++ Loterias/Loterias.Application/Utils/Settings/GameRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Loterias.Application.Utils.Settings
+{
+    public class GameRequest
+    {
+        public string Uri { get; set; }
+        public string KeyParam { get; set; }
+        public string ValueParam { get; set; }
+    }
+}
