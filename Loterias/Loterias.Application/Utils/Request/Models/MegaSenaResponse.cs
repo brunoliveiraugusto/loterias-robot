db4a8fee5882f8bfd91a6ce8e28eead588e3b129
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-07 20:26:45 -0300

    Criação de modelo de responde.

---
+++ Loterias/Loterias.Application/Utils/Request/Models/MegaSenaResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace Loterias.Application.Utils.Request.Models
+{
+    public class MegaSenaResponse
+    {
+        [JsonProperty("html")]
+        public string Html { get; set; }
+    }
+}
