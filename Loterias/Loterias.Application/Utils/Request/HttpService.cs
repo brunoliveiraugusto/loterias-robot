4841723c2dc59dc62f990bb7b9518bb13110c633
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-07 20:26:32 -0300

    Criação de serviço para realização de chamadas http.

---
+++ Loterias/Loterias.Application/Utils/Request/HttpService.cs
@@ -0,0 +1,47 @@
+﻿using Loterias.Application.Utils.Request.Interfaces;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loterias.Application.Utils.Request
+{
+    public class HttpService : IHttpService
+    {
+        private readonly HttpClient _httpClient;
+
+        public HttpService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<T> Get<T>(string endpoint, Dictionary<string, string> requestParams) where T : class
+        {
+            try
+            {
+                string urlRequest = BuildUrlRequest(endpoint, requestParams);
+                HttpResponseMessage response = await _httpClient.GetAsync(urlRequest);
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(responseBody);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        private string BuildUrlRequest(string endpoint, Dictionary<string, string> requestParams)
+        {
+            StringBuilder urlRequest = new StringBuilder();
+
+            foreach (var requestParam in requestParams)
+            {
+                urlRequest.Append(endpoint).Append('?').Append(requestParam.Key).Append('=').Append(requestParam.Value);
+            }
+
+            return urlRequest.ToString();
+        }
+    }
+}
---
+++ Loterias/Loterias.Application/Utils/Request/Interfaces/IHttpService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Loterias.Application.Utils.Request.Interfaces
+{
+    public interface IHttpService
+    {
+        Task<T> Get<T>(string endpoint, Dictionary<string, string> requestParams) where T : class;
+    }
+}
