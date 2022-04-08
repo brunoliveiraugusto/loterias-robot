2df1c77b1bd2de0fdc4e3059b836ef4b05c454ab
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-08 15:47:08 -0300

    Criação de métodos para atualização, inserção de csv e extração da última data de sorteio.

--- Loterias/Loterias.Application/Services/CsvService.cs
+++ Loterias/Loterias.Application/Services/CsvService.cs
@@ -1,39 +1,103 @@
-﻿using Loterias.Application.Models;
+﻿using CsvHelper;
+using CsvHelper.Configuration;
+using Loterias.Application.Models;
 using Loterias.Application.Services.Interfaces;
+using Loterias.Application.Utils.Exceptions;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
+using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Loterias.Application.Services
 {
     public class CsvService : ICsvService
     {
+        private readonly CsvConfiguration _config = new(CultureInfo.InvariantCulture) { Delimiter = ";", HasHeaderRecord = false };
         private const string PATH_CSV = "C:\\Projetos\\Pessoal\\Loterias\\loterias-robot\\Loterias\\Loterias.Application\\FilesCsv\\MS.csv";
 
         public async Task<IEnumerable<Game>> Read()
         {
-            var data = await File.ReadAllLinesAsync(PATH_CSV);
-            List<Game> games = new();
-
-            foreach (var line in data)
+            try
             {
-                Game game = line;
-                games.Add(game);
-            }
+                var data = await File.ReadAllLinesAsync(PATH_CSV);
+                List<Game> games = new();
 
-            return games;
+                foreach (var line in data)
+                {
+                    Game game = line;
+                    games.Add(game);
+                }
+
+                return games;
+            }
+            catch
+            {
+                throw new ErrorRetrievingDataException();
+            }
         }
 
         public async Task Update(IEnumerable<Game> games)
         {
-            if(File.Exists(PATH_CSV))
+            try
             {
-                var data = await File.ReadAllLinesAsync(PATH_CSV);
-                DateTime.TryParse(data[0].Split(";")[0], out DateTime result);
-                DateTime lastDateDrawn = result;
+                if (File.Exists(PATH_CSV))
+                {
+                    var data = await File.ReadAllLinesAsync(PATH_CSV);
+                    DateTime? lastDateDrawn = ExtractDateLastDraw(data);
+                    IEnumerable<Game> gamesToBeInserted = lastDateDrawn != null ? games.Where(game => game.DrawDate > lastDateDrawn) : games;
+                    await UpdateExistsFile(gamesToBeInserted);
+                    return;
+                }
+                
+                await Insert(games);
+            }
+            catch
+            {
+                throw;
+            }
+        }
 
-                //TODO: pego a última data sorteada e pego na lista de games os games a partir daquela data para mais recente
+        private async Task Insert(IEnumerable<Game> games)
+        {
+            try
+            {
+                using var writer = new StreamWriter(PATH_CSV, false, Encoding.UTF8);
+                using var csv = new CsvWriter(writer, _config);
+                await csv.WriteRecordsAsync(games);
+            }
+            catch
+            {
+                throw new ErrorEnteringDataException("Error inserting data in csv.");
+            }
+        }
+
+        private async Task UpdateExistsFile(IEnumerable<Game> games)
+        {
+            try
+            {
+                using var writer = new StreamWriter(PATH_CSV, true);
+                using var csv = new CsvWriter(writer, _config);
+                await csv.WriteRecordsAsync(games);
+            }
+            catch
+            {
+                throw new ErrorEnteringDataException("Error updating data in csv.");
+            }
+        }
+
+        private DateTime? ExtractDateLastDraw(string[] games)
+        {
+            try
+            {
+                DateTime.TryParse(games[0].Split(";")[0], out DateTime result);
+                return result;
+            }
+            catch
+            {
+                return null;
             }
         }
     }
