cce0765d1e83c7cfc18a96e10fa1ba892310a396
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-08 15:48:42 -0300

    Criação de exceção para manipulação de dados csv.

---
+++ Loterias/Loterias.Application/Utils/Exceptions/ErrorEnteringDataException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Loterias.Application.Utils.Exceptions
+{
+    public class ErrorEnteringDataException : Exception
+    {
+        public ErrorEnteringDataException(string message) : base(message)
+        {
+
+        }
+    }
+}
---
+++ Loterias/Loterias.Application/Utils/Exceptions/ErrorRetrievingDataException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Loterias.Application.Utils.Exceptions
+{
+    public class ErrorRetrievingDataException : Exception
+    {
+        public ErrorRetrievingDataException(string message = "Error retrieving data from csv") : base(message)
+        {
+
+        }
+    }
+}
