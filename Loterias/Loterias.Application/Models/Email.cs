9295f40a5e6bd0a47ff663593e7a8e5f859002a4
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-13 09:59:48 -0300

    Criação de modelos.

---
+++ Loterias/Loterias.Application/Models/Email.cs
@@ -0,0 +1,21 @@
+﻿namespace Loterias.Application.Models
+{
+    public class Email
+    {
+        public string Name { get; set; }
+        public string Recipient { get; set; }
+        public string Subject { get; set; }
+        public string PlainTextContext { get; set; }
+        public string HtmlContent { get; set; }
+        public static Email NewEmail(string name, string recipient, string subject, string plainTextContext, string htmlContent) => new(name, recipient, subject, plainTextContext, htmlContent);
+
+        private Email(string name, string recipient, string subject, string plainTextContext, string htmlContent)
+        {
+            Name = name;
+            Recipient = recipient;
+            Subject = subject;
+            PlainTextContext = plainTextContext;
+            HtmlContent = htmlContent;
+        }
+    }
+}
---
+++ Loterias/Loterias.Application/Models/RecommendedGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Loterias.Application.Models
+{
+    public class RecommendedGame
+    {
+        public IEnumerable<string> Numbers { get; set; }
+        public DateTime NextDrawDate { get; set; }
+        public IEnumerable<PossibleGame> PossibleGames { get; set; }
+
+        public static RecommendedGame CreateRecommendedGame(List<string> numbers, DateTime nextDrawDate, List<PossibleGame> possibleGames)
+        {
+            return new RecommendedGame
+            {
+                Numbers = numbers,
+                NextDrawDate = nextDrawDate,
+                PossibleGames = possibleGames
+            };
+        }
+    }
+}
