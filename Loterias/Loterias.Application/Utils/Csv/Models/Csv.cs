c2f1b9d6dbc0e37da6315c22e4576762f851d69c
Bruno Augusto <brunoaugustoliveira@gmail.com>
2022-04-18 09:35:57 -0300

    Alteração de obtenção de valor.

--- Loterias/Loterias.Application/Utils/Csv/Models/Csv.cs
+++ Loterias/Loterias.Application/Utils/Csv/Models/Csv.cs
@@ -10,34 +10,59 @@ namespace Loterias.Application.Utils.Csv.Models
     {
         [Index(0)]
         public string DrawDate { get; set; }
+        
         [Index(1)]
         public string NumberOne { get; set; }
+        
         [Index(2)]
         public string NumberTwo { get; set; }
+        
         [Index(3)]
         public string NumberThree { get; set; }
+        
         [Index(4)]
         public string NumberFour { get; set; }
+        
         [Index(5)]
         public string NumberFive { get; set; }
+        
         [Index(6)]
         public string NumberSix { get; set; }
+        
+        //[Index(7), Optional]
+        //public string NumberSeven { get; set; }
+        //[Index(8), Optional]
+        //public string NumberEight { get; set; }
+        //[Index(9), Optional]
+        //public string NumberNine { get; set; }
+        //[Index(10), Optional]
+        //public string NumberTen { get; set; }
+        //[Index(11), Optional]
+        //public string NumberEleven { get; set; }
+        //[Index(12), Optional]
+        //public string NumberTwelve { get; set; }
+        //[Index(13), Optional]
+        //public string NumberThirteen { get; set; }
+        //[Index(14), Optional]
+        //public string NumberFourteen { get; set; }
+        //[Index(15), Optional]
+        //public string NumberFifteen { get; set; }
 
         public static implicit operator Csv(List<HtmlNode> gameDrawn)
         {
-            //TODO: INCLUIR FOR PARA PREENCHIMENTO DOS CAMPOS COM BASE NA VARIÁVEL INDICATIVA DA QUANTIDADE DE NÚMEROS DE JOGOS DO APPSETTINGS
             DateTime.TryParse(gameDrawn.ElementAt(0).InnerText, out DateTime result);
             DateTime drawDate = result;
 
             return new Csv
             {
                 DrawDate = drawDate.Date.ToShortDateString(),
-                NumberOne = gameDrawn[1].InnerText.TrimStart('0'),
-                NumberTwo = gameDrawn[2].InnerText.TrimStart('0'),
-                NumberThree = gameDrawn[3].InnerText.TrimStart('0'),
-                NumberFour = gameDrawn[4].InnerText.TrimStart('0'),
-                NumberFive = gameDrawn[5].InnerText.TrimStart('0'),
-                NumberSix = gameDrawn[6].InnerText.TrimStart('0'),
+                NumberOne = gameDrawn.ElementAtOrDefault(1)?.InnerText.TrimStart('0'),
+                NumberTwo = gameDrawn.ElementAtOrDefault(2)?.InnerText.TrimStart('0'),
+                NumberThree = gameDrawn.ElementAtOrDefault(3)?.InnerText.TrimStart('0'),
+                NumberFour = gameDrawn.ElementAtOrDefault(4)?.InnerText.TrimStart('0'),
+                NumberFive = gameDrawn.ElementAtOrDefault(5)?.InnerText.TrimStart('0'),
+                NumberSix = gameDrawn.ElementAtOrDefault(6)?.InnerText.TrimStart('0'),
+
             };
         }
     }
